3e47ed9990f85718be6655885386126b01b72f5f
Marina Kravchenko <marishka-k87@yandex.ru>
2023-05-02 22:07:49 +0300

    backend

--- backend/src/ecosystem.config.js
+++ backend/src/ecosystem.config.js
@@ -1,33 +1,33 @@
-const dotenv = require("dotenv");
+const dotenv = require('dotenv');
 
 dotenv.config({
-  path: ".env.deploy",
+  path: '.env.deploy',
 });
 
 const {
   DEPLOY_USER,
   DEPLOY_HOST,
   DEPLOY_PATH,
-  DEPLOY_REF = "origin/master",
+  DEPLOY_REF = 'origin/master',
 } = process.env;
 
 module.exports = {
   apps: [
     {
-      name: "mesto-backend",
-      script: "./dist/app.js",
+      name: 'mesto-backend',
+      script: './dist/app.js',
     },
   ],
 
   deploy: {
     production: {
-      user: "kramarvik",
-      host: "51.250.13.190",
-      ref: "origin/master",
-      repo: "https://github.com/marishka-k/web-plus-pm2-deploy.git",
-      path: "/home/kramarvik/web-plus-pm2-deploy/backend",
-      "post-deploy":
-        "cd backend && npm i && npm run build && pm2 startOrRestart ecosystem.config.js",
+      user: DEPLOY_USER,
+      host: DEPLOY_HOST,
+      ref: DEPLOY_REF,
+      repo: 'https://github.com/marishka-k/web-plus-pm2-deploy.git',
+      path: DEPLOY_PATH,
+      'pre-deploy-local': `scp ./.env ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}/source/backend`,
+      'post-deploy': `cd ${DEPLOY_PATH}/source/backend && npm i && npm run build && pm2 restart all`,
     },
   },
 };
