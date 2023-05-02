8eab603d515f46d51a24e93d936cd96cf1c2dec3
Marina Kravchenko <marishka-k87@yandex.ru>
2023-05-02 23:23:43 +0300

    +

--- frontend/ecosystem.config.js
+++ frontend/ecosystem.config.js
@@ -18,12 +18,13 @@ module.exports = {
 
   deploy: {
     production: {
-      user: "kramarvik",
-      host: "51.250.13.190",
-      ref: "origin/master",
+      user: DEPLOY_USER,
+      host: DEPLOY_HOST,
+      ref: DEPLOY_REF,
       repo: "https://github.com/marishka-k/web-plus-pm2-deploy.git",
-      path: "/home/kramarvik/web-plus-pm2-deploy/frontend",
-      "post-deploy": "cd frontend && npm i && npm run build"
+      path: DEPLOY_PATH,
+      'pre-deploy-local': `scp ./.env ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}/source/backend`,
+      "post-deploy": "cd ~/mesto-frontend && npm i && npm run build"
     },
   },
 };
