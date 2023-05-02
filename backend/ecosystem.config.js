ba45d1a5056d4f06989a873ad257be245742cd89
Marina Kravchenko <marishka-k87@yandex.ru>
2023-05-02 23:54:23 +0300

    +

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -9,6 +9,7 @@ const {
   DEPLOY_HOST,
   DEPLOY_PATH,
   DEPLOY_REF = 'origin/master',
+  DEPLOY_REPO,
 } = process.env;
 
 module.exports = {
@@ -24,10 +25,10 @@ module.exports = {
       user: DEPLOY_USER,
       host: DEPLOY_HOST,
       ref: DEPLOY_REF,
-      repo: 'https://github.com/marishka-k/web-plus-pm2-deploy.git',
+      repo: DEPLOY_REPO,
       path: DEPLOY_PATH,
       'pre-deploy-local': `scp ./.env ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}/source/backend`,
-      'post-deploy': `cd ${DEPLOY_PATH}/source/backend && npm i && npm run build && pm2 restart all`,
+      'post-deploy': 'cd backend && npm i && npm run build && pm2 startOrRestart ecosystem.config.js --env production',
     },
   },
 };
