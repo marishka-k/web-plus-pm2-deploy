8410ec98455e329ad96fc36db11a6cf134676dae
Marina Kravchenko <marishka-k87@yandex.ru>
2023-05-03 00:12:09 +0300

    fix

--- frontend/ecosystem.config.js
+++ frontend/ecosystem.config.js
@@ -6,6 +6,7 @@ const {
   DEPLOY_HOST,
   DEPLOY_PATH,
   DEPLOY_REF = "origin/master",
+  DEPLOY_REPO,
 } = process.env;
 
 module.exports = {
@@ -21,10 +22,10 @@ module.exports = {
       user: DEPLOY_USER,
       host: DEPLOY_HOST,
       ref: DEPLOY_REF,
-      repo: "https://github.com/marishka-k/web-plus-pm2-deploy.git",
+      repo: DEPLOY_REPO,
       path: DEPLOY_PATH,
-      'pre-deploy-local': `scp ./.env ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}/source/backend`,
-      "post-deploy": "cd ~/mesto-frontend && npm i && npm run build"
+      "pre-deploy-local": `scp ./.env.deploy ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}`,
+      "post-deploy": "cd ~/frontend && npm i && npm run build",
     },
   },
 };
